e9ad5a100a9c9b6f75202a2dd5263fd7bfbfaf20
calvi <calvin@brianandkathi.com>
2023-02-11 18:39:46 -0500

    Implemented DamageDeliveryTest.

---
+++ src/test/java/org/rpgl/subevent/DamageDeliveryTest.java
@@ -0,0 +1,75 @@
+package org.rpgl.subevent;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.rpgl.core.RPGLContext;
+import org.rpgl.datapack.DatapackLoader;
+import org.rpgl.datapack.DatapackTest;
+import org.rpgl.exception.SubeventMismatchException;
+import org.rpgl.json.JsonObject;
+import org.rpgl.uuidtable.UUIDTable;
+
+import java.io.File;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DamageDeliveryTest {
+
+    @BeforeAll
+    static void beforeAll() throws Exception {
+        DatapackLoader.loadDatapacks(
+                new File(Objects.requireNonNull(DatapackTest.class.getClassLoader().getResource("datapacks")).toURI())
+        );
+    }
+
+    @AfterAll
+    static void afterAll() {
+        DatapackLoader.DATAPACKS.clear();
+    }
+
+    @AfterEach
+    void afterEach() {
+        UUIDTable.clear();
+    }
+
+    @Test
+    @DisplayName("invoke wrong subevent")
+    void invoke_wrongSubevent_throwsException() {
+        DamageDelivery damageDelivery = new DamageDelivery();
+        damageDelivery.joinSubeventData(new JsonObject() {{
+            /*{
+                "subevent": "not_a_subevent"
+            }*/
+            this.putString("subevent", "not_a_subevent");
+        }});
+
+        assertThrows(SubeventMismatchException.class,
+                () -> damageDelivery.invoke(new RPGLContext()),
+                "Subevent should throw a SubeventMismatchException if the specified subevent doesn't match"
+        );
+    }
+
+    @Test
+    @DisplayName("getDamage returns stored damage values")
+    void getDamage_returnsStoredDamageValues() {
+        DamageDelivery damageDelivery = new DamageDelivery();
+        damageDelivery.joinSubeventData(new JsonObject() {{
+            this.putJsonObject("damage", new JsonObject() {{
+                this.putInteger("fire", 10);
+                this.putInteger("cold", 10);
+            }});
+        }});
+
+        String expected = """
+                {"cold":10,"fire":10}""";
+        assertEquals(expected, damageDelivery.getDamage().toString(),
+                ""
+        );
+    }
+
+}
