2e852cd0a3f429b086e9fc4ea5159db061f74903
calvi <calvin@brianandkathi.com>
2023-02-25 13:13:14 -0500

    Implemented ContestTest.
    Code cleanup.

--- src/main/java/org/rpgl/subevent/Contest.java
+++ src/main/java/org/rpgl/subevent/Contest.java
@@ -1,12 +1,17 @@
 package org.rpgl.subevent;
 
 import org.rpgl.core.RPGLContext;
+import org.rpgl.exception.ContestTargetFormatException;
 import org.rpgl.json.JsonArray;
 import org.rpgl.json.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 
 public class Contest extends Subevent {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Contest.class);
 
     public Contest() {
         super("contest");
@@ -30,9 +35,15 @@ public class Contest extends Subevent {
 
     @Override
     public void prepare(RPGLContext context) throws Exception {
-        // die wins on a tie against a DC / save DC, but not against another ability check
-        boolean mustExceedTarget = (this.subeventJson.getInteger("target") != null)
-                || (this.subeventJson.getJsonObject("target").getString("subevent").equals("calculate_save_difficulty_class"));
+        super.prepare(context);
+        // when target is a static value or a save difficulty class, source wins by matching or exceeding target.
+        // when target is an ability check, source wins only by exceeding target.
+        boolean mustExceedTarget;
+        try {
+          mustExceedTarget = this.subeventJson.getJsonObject("target_contest").getString("subevent").equals("ability_check");
+        } catch (NullPointerException e) {
+            mustExceedTarget = false;
+        }
         this.subeventJson.putBoolean("must_exceed_target", mustExceedTarget);
     }
 
@@ -41,7 +52,9 @@ public class Contest extends Subevent {
         super.invoke(context);
         int sourceResult = this.getSourceResult(context);
         int targetResult = this.getTargetResult(context);
-        targetResult += this.subeventJson.getBoolean("must_exceed_target") ? 1 : 0;
+        if (this.subeventJson.getBoolean("must_exceed_target")) {
+            targetResult++;
+        }
 
         if (sourceResult >= targetResult) {
             this.resolveNestedSubevents("source_wins", context);
@@ -52,29 +65,32 @@ public class Contest extends Subevent {
 
     int getSourceResult(RPGLContext context) throws Exception {
         AbilityCheck abilityCheck = new AbilityCheck();
-        abilityCheck.joinSubeventData(this.subeventJson.getJsonObject("source"));
-        abilityCheck.subeventJson.getJsonArray("tags").asList().addAll(this.subeventJson.getJsonArray("tags").asList());
+        abilityCheck.joinSubeventData(this.subeventJson.getJsonObject("source_contest"));
         abilityCheck.setSource(this.getSource());
         abilityCheck.prepare(context);
+        abilityCheck.subeventJson.getJsonArray("tags").asList().addAll(this.subeventJson.getJsonArray("tags").asList());
         abilityCheck.setTarget(this.getSource());
         abilityCheck.invoke(context);
         return abilityCheck.get();
     }
 
     int getTargetResult(RPGLContext context) throws Exception {
-        Integer targetResult = this.subeventJson.getInteger("target");
+        Integer targetResult = this.subeventJson.getInteger("target_contest");
         if (targetResult != null) {
             return targetResult;
         }
 
-        JsonObject targetJson = this.subeventJson.getJsonObject("target");
-        if (targetJson.getString("subevent").equals("calculate_save_difficulty_class")) {
-            return this.getTargetResultAsSaveDifficultyClass(targetJson, context);
-        } else if (targetJson.getString("subevent").equals("ability_check")) {
-            return this.getTargetResultAsAbilityCheck(targetJson, context);
+        JsonObject targetContestJson = this.subeventJson.getJsonObject("target_contest");
+        String subeventId = targetContestJson.getString("subevent");
+        if (subeventId.equals("calculate_save_difficulty_class")) {
+            return this.getTargetResultAsSaveDifficultyClass(targetContestJson, context);
+        } else if (subeventId.equals("ability_check")) {
+            return this.getTargetResultAsAbilityCheck(targetContestJson, context);
         }
 
-        throw new Exception("contest target data invalid"); // TODO handle this better
+        ContestTargetFormatException e = new ContestTargetFormatException(subeventId);
+        LOGGER.error(e.getMessage());
+        throw e;
     }
 
     int getTargetResultAsSaveDifficultyClass(JsonObject targetJson, RPGLContext context) throws Exception {
@@ -90,9 +106,9 @@ public class Contest extends Subevent {
     int getTargetResultAsAbilityCheck(JsonObject targetJson, RPGLContext context) throws Exception {
         AbilityCheck abilityCheck = new AbilityCheck();
         abilityCheck.joinSubeventData(targetJson);
-        abilityCheck.subeventJson.getJsonArray("tags").asList().addAll(this.subeventJson.getJsonArray("tags").asList());
         abilityCheck.setSource(this.getTarget());
         abilityCheck.prepare(context);
+        abilityCheck.subeventJson.getJsonArray("tags").asList().addAll(this.subeventJson.getJsonArray("tags").asList());
         abilityCheck.setTarget(this.getTarget());
         abilityCheck.invoke(context);
         return abilityCheck.get();
