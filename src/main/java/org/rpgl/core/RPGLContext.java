efba4783a83e9a37a0672978ff6a23bddef4c346
calvi <calvin@brianandkathi.com>
2023-03-09 20:36:52 -0500

    First attempt at tracking initiative within RPGLContext objects.

--- src/main/java/org/rpgl/core/RPGLContext.java
+++ src/main/java/org/rpgl/core/RPGLContext.java
@@ -1,8 +1,13 @@
 package org.rpgl.core;
 
+import org.rpgl.json.JsonArray;
+import org.rpgl.json.JsonObject;
+import org.rpgl.subevent.AbilityCheck;
 import org.rpgl.subevent.Subevent;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -18,31 +23,62 @@ public class RPGLContext {
 
     private final Map<String, RPGLObject> contextObjects;
 
+    private final Map<Double, RPGLObject> turnOrder;
+
+    private Double currentInitiative;
+
     /**
      * Default constructor for RPGLContext. This is the typical way a RPGLContext object is meant to be constructed.
      */
     public RPGLContext() {
         this.contextObjects = new HashMap<>();
+        this.turnOrder = new HashMap<>();
+        this.currentInitiative = null;
     }
 
     /**
-     * This method adds a RPGLObject to the context.
+     * This method adds a RPGLObject to the context. The passed object will be assigned an initiative score and added to
+     * the turn order. This initiative accounts for dexterity scores, and assigns random bonuses to differentiate what
+     * would otherwise result in a tie.
      *
      * @param object an RPGLObject to be added into the context
      */
-    public void add(RPGLObject object) {
+    public void add(RPGLObject object) throws Exception {
         String objectUuid = object.getUuid();
         this.contextObjects.putIfAbsent(objectUuid, object);
+        Double initiative = this.rollObjectInitiative(object);
+        turnOrder.put(initiative, object);
     }
 
     /**
-     * This method removes a RPGLObject from the context.
+     * This method adds a RPGLObject to the context, using a pre-determined initiative value. This method should only be
+     * used during testing, as it risks overriding existing initiative scores if the same initiative is passed more than
+     * once.
+     *
+     * @param object an RPGLObject to be added into the context
+     */
+    public void add(RPGLObject object, double initiative) throws Exception {
+        String objectUuid = object.getUuid();
+        this.contextObjects.putIfAbsent(objectUuid, object);
+        turnOrder.put(initiative, object);
+    }
+
+    /**
+     * This method removes a RPGLObject from the context. This includes removing it from context as well as forgetting
+     * its place in the initiative order.
      *
      * @param objectUuid the UUID of an RPGLObject to be removed from context
      * @return the RPGLObject removed from context
      */
     public RPGLObject remove(String objectUuid) {
-        return this.contextObjects.remove(objectUuid);
+        RPGLObject object = this.contextObjects.remove(objectUuid);
+        for (Map.Entry<Double, RPGLObject> entry : this.turnOrder.entrySet()) {
+            if (entry.getValue() == object) {
+                this.turnOrder.remove(entry.getKey());
+                break;
+            }
+        }
+        return object;
     }
 
     /**
@@ -61,6 +97,42 @@ public class RPGLContext {
                 wasProcessed |= contextObjectsEntry.getValue().processSubevent(subevent, context);
             }
         } while (wasProcessed);
+    }
+
+    double rollObjectInitiative(RPGLObject object) throws Exception {
+        AbilityCheck abilityCheck = new AbilityCheck();
+        abilityCheck.joinSubeventData(new JsonObject() {{
+            /*{
+                "subevent": "ability_check",
+                "ability": "dex",
+                "tags": [ "initiative" ]
+            }*/
+            this.putString("subevent", "ability_check");
+            this.putString("ability", "dex");
+            this.putJsonArray("tags", new JsonArray() {{
+                this.addString("initiative");
+            }});
+        }});
+        abilityCheck.setSource(object);
+        abilityCheck.prepare(this);
+        abilityCheck.setTarget(object);
+        abilityCheck.invoke(this);
+        // initiative = ability check + (ability score / 100) + (random bonus / 10,000)
+        return abilityCheck.get()
+                + ( (double) object.getAbilityScoreFromAbilityName("dex", this) / 100)
+                + (Math.random() / 10000);
+    }
+
+    public RPGLObject nextObject() {
+        List<Double> initiativeScores = new ArrayList<>(turnOrder.keySet().stream().sorted().toList());
+        int initiativeIndex = initiativeScores.indexOf(this.currentInitiative);
+        if (initiativeIndex == initiativeScores.size() - 1) {
+            initiativeIndex = 0;
+        } else {
+            initiativeIndex++;
+        }
+        this.currentInitiative = initiativeScores.get(initiativeIndex);
+        return turnOrder.get(this.currentInitiative);
     }
 
     @Override
