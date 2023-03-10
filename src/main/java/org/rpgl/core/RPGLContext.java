54feb399fbd80beb0b8accba1bea55a0d260c6ed
calvi <calvin@brianandkathi.com>
2023-03-10 17:39:42 -0500

    Implemented IsObjectsTurn Condition.
    Additional support for RPGLContext turn manipulation.
    
    Added javadocs.

--- src/main/java/org/rpgl/core/RPGLContext.java
+++ src/main/java/org/rpgl/core/RPGLContext.java
@@ -6,6 +6,8 @@ import org.rpgl.subevent.AbilityCheck;
 import org.rpgl.subevent.Subevent;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,13 +29,28 @@ public class RPGLContext {
 
     private Double currentInitiative;
 
-    /**
-     * Default constructor for RPGLContext. This is the typical way a RPGLContext object is meant to be constructed.
-     */
     public RPGLContext() {
         this.contextObjects = new HashMap<>();
         this.turnOrder = new HashMap<>();
         this.currentInitiative = null;
+    }
+
+    /**
+     * Inline constructor for RPGLContext class. This constructor is meant to be used when a temporary RPGLContext is
+     * being created. This temporary RPGLContext should not be used to track turn order, and only exists in order to
+     * facilitate a RPGLEvent which spans an area which is much larger than an existing RPGLContext would account for.
+     * For example, this constructor may be used to include all RPGLObjects in a 1-mile radius of the center of a large
+     * area-of-effect spell, even though the spell was cast from within a much smaller RPGLContext.
+     *
+     * @param objects a collection of RPGLObjects to be included in this context
+     */
+    public RPGLContext(Collection<RPGLObject> objects) {
+        this.contextObjects = new HashMap<>();
+        this.turnOrder = new HashMap<>();
+        this.currentInitiative = null;
+        objects.forEach(object -> {
+            contextObjects.put(object.getUuid(), object);
+        });
     }
 
     /**
@@ -55,7 +72,8 @@ public class RPGLContext {
      * used during testing, as it risks overriding existing initiative scores if the same initiative is passed more than
      * once.
      *
-     * @param object an RPGLObject to be added into the context
+     * @param object     an RPGLObject to be added into the context
+     * @param initiative an initiative score
      */
     public void add(RPGLObject object, double initiative) throws Exception {
         String objectUuid = object.getUuid();
@@ -99,40 +117,86 @@ public class RPGLContext {
         } while (wasProcessed);
     }
 
+    /**
+     * This helper method is used to roll initiative for an RPGLObject being added to this RPGLContext. In testing mode,
+     * the first two d20 rolls entailed by this check will always be 10's. An initiative roll is determined by the
+     * following equation:
+     * <br>
+     * <br>
+     * <code>initiative = dex ability check + (dex score / 100) + (random bonus / 10,000)</code>
+     *
+     * @param object an RPGLObject whose initiative is being rolled
+     * @return an initiative score for object
+     *
+     * @throws Exception if an exception occurs
+     */
     double rollObjectInitiative(RPGLObject object) throws Exception {
         AbilityCheck abilityCheck = new AbilityCheck();
         abilityCheck.joinSubeventData(new JsonObject() {{
             /*{
                 "subevent": "ability_check",
                 "ability": "dex",
-                "tags": [ "initiative" ]
+                "tags": [ "initiative" ],
+                "determined": [ 10, 10 ]
             }*/
             this.putString("subevent", "ability_check");
             this.putString("ability", "dex");
             this.putJsonArray("tags", new JsonArray() {{
                 this.addString("initiative");
             }});
+            this.putJsonArray("determined", new JsonArray() {{
+                this.addInteger(10);
+                this.addInteger(10);
+            }});
         }});
         abilityCheck.setSource(object);
         abilityCheck.prepare(this);
         abilityCheck.setTarget(object);
         abilityCheck.invoke(this);
-        // initiative = ability check + (ability score / 100) + (random bonus / 10,000)
+
         return abilityCheck.get()
                 + ( (double) object.getAbilityScoreFromAbilityName("dex", this) / 100)
                 + (Math.random() / 10000);
     }
 
-    public RPGLObject nextObject() {
-        List<Double> initiativeScores = new ArrayList<>(turnOrder.keySet().stream().sorted().toList());
+    /**
+     * Returns the RPGLObject which is active in the current turn.
+     *
+     * @return an RPGLObject
+     */
+    public RPGLObject currentObject() {
+        return turnOrder.get(this.currentInitiative);
+    }
+
+    /**
+     * This method transitions to the turn of the next RPGLObject, in order of descending initiative. If this method has
+     * not been called on this RPGLContext yet, it returns the RPGLObject with the highest initiative score. This method
+     * also facilitates the ending of turns and the beginning of turns, as appropriate. If the current initiative score
+     * is the lowest initiative in context, this method will loop back to the highest initiative score and start the
+     * cycle over again. This method returns the RPGLObject which is active in the new turn.
+     *
+     * @return an RPGLObject
+     */
+    public RPGLObject nextObject() throws Exception {
+        List<Double> initiativeScores = new ArrayList<>(this.turnOrder.keySet().stream().sorted().toList());
+        Collections.reverse(initiativeScores);
+        RPGLObject lastObject = this.turnOrder.get(this.currentInitiative);
         int initiativeIndex = initiativeScores.indexOf(this.currentInitiative);
         if (initiativeIndex == initiativeScores.size() - 1) {
+            // loop back around to the highest initiative in context
             initiativeIndex = 0;
         } else {
             initiativeIndex++;
         }
         this.currentInitiative = initiativeScores.get(initiativeIndex);
-        return turnOrder.get(this.currentInitiative);
+        RPGLObject nextObject = this.turnOrder.get(this.currentInitiative);
+        if (lastObject != null) {
+            // end turn of last object, unless this is the first turn
+            lastObject.endTurn(this);
+        }
+        // start turn of next object
+        nextObject.startTurn(this);
+        return nextObject;
     }
 
     @Override
