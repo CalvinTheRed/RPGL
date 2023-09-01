e130762b1a0875a6760608906c4bf42c1d3da31b
calvi <calvin@brianandkathi.com>
2023-09-01 11:30:05 -0400

    Implemented AbilityContestTest.

--- src/main/java/org/rpgl/subevent/AbilityContest.java
+++ src/main/java/org/rpgl/subevent/AbilityContest.java
@@ -46,46 +46,51 @@ public class AbilityContest extends Subevent {
 
     @Override
     public void run(RPGLContext context) throws Exception {
-        AbilityCheck sourceAbilityCheck = new AbilityCheck();
-        sourceAbilityCheck.joinSubeventData(new JsonObject() {{
-            this.putString("ability", json.seekString("source.ability"));
-            this.putString("skill", json.seekString("source.skill"));
-            this.putJsonArray("tags", new JsonArray(json.getJsonArray("tags").asList()));
-            this.putJsonArray("determined", json.seekJsonArray("source.determined"));
-        }});
-        sourceAbilityCheck.setSource(this.getTarget());
-        sourceAbilityCheck.prepare(context);
-        sourceAbilityCheck.setTarget(this.getSource());
-        sourceAbilityCheck.invoke(context);
+        int sourceAbilityCheck = this.getSourceAbilityCheck(context);
+        int targetAbilityCheck = this.getTargetAbilityCheck(context);
 
-        AbilityCheck targetAbilityCheck = new AbilityCheck();
-        targetAbilityCheck.joinSubeventData(new JsonObject() {{
-            this.putString("ability", json.seekString("target.ability"));
-            this.putString("skill", json.seekString("target.skill"));
-            this.putJsonArray("tags", new JsonArray() {{
-                this.asList().addAll(json.getJsonArray("tags").asList());
-                this.addString("ability_save");
-            }});
-            this.putJsonArray("determined", json.seekJsonArray("target.determined"));
-        }});
-        targetAbilityCheck.setSource(this.getSource());
-        targetAbilityCheck.prepare(context);
-        targetAbilityCheck.setTarget(this.getTarget());
-        targetAbilityCheck.invoke(context);
-
-        if (sourceAbilityCheck.get() < targetAbilityCheck.get()) {
+        if (sourceAbilityCheck < targetAbilityCheck) {
             this.resolveNestedSubevents("fail", context);
-        } else if (sourceAbilityCheck.get() > targetAbilityCheck.get()) {
+        } else if (sourceAbilityCheck > targetAbilityCheck) {
             this.resolveNestedSubevents("pass", context);
         }
-        // nothing happens on a tied ability contest
+    }
+
+    int getSourceAbilityCheck(RPGLContext context) throws Exception {
+        AbilityCheck abilityCheck = new AbilityCheck();
+        abilityCheck.joinSubeventData(new JsonObject() {{
+            this.putString("ability", json.seekString("source_check.ability"));
+            this.putString("skill", json.seekString("source_check.skill"));
+            this.putJsonArray("tags", new JsonArray(json.getJsonArray("tags").asList()));
+            this.putJsonArray("determined", json.seekJsonArray("source_check.determined"));
+        }});
+        abilityCheck.setSource(this.getTarget());
+        abilityCheck.prepare(context);
+        abilityCheck.setTarget(this.getSource());
+        abilityCheck.invoke(context);
+        return abilityCheck.get();
+    }
+
+    int getTargetAbilityCheck(RPGLContext context) throws Exception {
+        AbilityCheck abilityCheck = new AbilityCheck();
+        abilityCheck.joinSubeventData(new JsonObject() {{
+            this.putString("ability", json.seekString("target_check.ability"));
+            this.putString("skill", json.seekString("target_check.skill"));
+            this.putJsonArray("tags", new JsonArray(json.getJsonArray("tags").asList()));
+            this.putJsonArray("determined", json.seekJsonArray("target_check.determined"));
+        }});
+        abilityCheck.setSource(this.getSource());
+        abilityCheck.prepare(context);
+        abilityCheck.setTarget(this.getTarget());
+        abilityCheck.invoke(context);
+        return abilityCheck.get();
     }
 
     /**
-     * This helper method resolves any nested Subevents within this Subevent in accordance to whether <code>target</code>
-     * passed or failed its ability save.
+     * This helper method resolves any nested Subevents within this Subevent in accordance to whether
+     * <code>source</code> passed or failed its ability contest.
      *
-     * @param passOrFail a String indicating whether the ability save was passed or failed
+     * @param passOrFail a String indicating whether the ability contest was passed or failed
      * @param context the context this Subevent takes place in
      *
      * @throws Exception if an exception occurs.
---
+++ src/test/java/org/rpgl/subevent/AbilityContestTest.java
@@ -0,0 +1,494 @@
+package org.rpgl.subevent;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.rpgl.core.RPGLContext;
+import org.rpgl.core.RPGLCore;
+import org.rpgl.core.RPGLFactory;
+import org.rpgl.core.RPGLObject;
+import org.rpgl.datapack.DatapackLoader;
+import org.rpgl.datapack.DatapackTest;
+import org.rpgl.exception.SubeventMismatchException;
+import org.rpgl.json.JsonArray;
+import org.rpgl.json.JsonObject;
+import org.rpgl.testUtils.DummyContext;
+import org.rpgl.uuidtable.UUIDTable;
+
+import java.io.File;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AbilityContestTest {
+
+    @BeforeAll
+    static void beforeAll() throws Exception {
+        DatapackLoader.loadDatapacks(
+                new File(Objects.requireNonNull(DatapackTest.class.getClassLoader().getResource("datapacks")).toURI())
+        );
+        RPGLCore.initializeTesting();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        DatapackLoader.DATAPACKS.clear();
+    }
+
+    @AfterEach
+    void afterEach() {
+        UUIDTable.clear();
+        DummySubevent.resetCounter();
+    }
+
+    @Test
+    @DisplayName("invoke wrong subevent")
+    void invoke_wrongSubevent_throwsException() {
+        Subevent subevent = new AbilityContest();
+        subevent.joinSubeventData(new JsonObject() {{
+            /*{
+                "subevent": "not_a_subevent"
+            }*/
+            this.putString("subevent", "not_a_subevent");
+        }});
+
+        assertThrows(SubeventMismatchException.class,
+                () -> subevent.invoke(new DummyContext()),
+                "Subevent should throw a SubeventMismatchException if the specified subevent doesn't match"
+        );
+    }
+
+    @Test
+    @DisplayName("resolveNestedSubevents invokes pass subevents (on pass)")
+    void resolveNestedSubevents_invokesPassSubevents_onPass() throws Exception {
+        RPGLObject source = RPGLFactory.newObject("debug:dummy");
+        RPGLObject target = RPGLFactory.newObject("debug:dummy");
+        RPGLContext context = new DummyContext();
+        context.add(source);
+        context.add(target);
+
+        AbilityContest abilityContest = new AbilityContest();
+        abilityContest.joinSubeventData(new JsonObject() {{
+            this.putJsonArray("pass", new JsonArray() {{
+                this.addJsonObject(new JsonObject() {{
+                    this.putString("subevent", "dummy_subevent");
+                }});
+            }});
+        }});
+        abilityContest.setSource(source);
+        abilityContest.setTarget(target);
+
+        abilityContest.resolveNestedSubevents("pass", context);
+
+        assertEquals(1, DummySubevent.counter,
+                "dummy subevent should be invoked on pass"
+        );
+    }
+
+    @Test
+    @DisplayName("resolveNestedSubevents does not invoke fail subevents (on pass)")
+    void resolveNestedSubevents_doesNotInvokeFailSubevents_onPass() throws Exception {
+        RPGLObject source = RPGLFactory.newObject("debug:dummy");
+        RPGLObject target = RPGLFactory.newObject("debug:dummy");
+        RPGLContext context = new DummyContext();
+        context.add(source);
+        context.add(target);
+
+        AbilityContest abilityContest = new AbilityContest();
+        abilityContest.joinSubeventData(new JsonObject() {{
+            this.putJsonArray("fail", new JsonArray() {{
+                this.addJsonObject(new JsonObject() {{
+                    this.putString("subevent", "dummy_subevent");
+                }});
+            }});
+        }});
+        abilityContest.setSource(source);
+        abilityContest.setTarget(target);
+
+        abilityContest.resolveNestedSubevents("pass", context);
+
+        assertEquals(0, DummySubevent.counter,
+                "dummy subevent should not be invoked on fail"
+        );
+    }
+
+    @Test
+    @DisplayName("resolveNestedSubevents invokes fail subevents (on fail)")
+    void resolveNestedSubevents_invokesFailSubevents_onFail() throws Exception {
+        RPGLObject source = RPGLFactory.newObject("debug:dummy");
+        RPGLObject target = RPGLFactory.newObject("debug:dummy");
+        RPGLContext context = new DummyContext();
+        context.add(source);
+        context.add(target);
+
+        AbilityContest abilityContest = new AbilityContest();
+        abilityContest.joinSubeventData(new JsonObject() {{
+            this.putJsonArray("fail", new JsonArray() {{
+                this.addJsonObject(new JsonObject() {{
+                    this.putString("subevent", "dummy_subevent");
+                }});
+            }});
+        }});
+        abilityContest.setSource(source);
+        abilityContest.setTarget(target);
+
+        abilityContest.resolveNestedSubevents("fail", context);
+
+        assertEquals(1, DummySubevent.counter,
+                "dummy subevent should be invoked on fail"
+        );
+    }
+
+    @Test
+    @DisplayName("resolveNestedSubevents does not invoke pass subevents (on fail)")
+    void resolveNestedSubevents_doesNotInvokePassSubevents_onFail() throws Exception {
+        RPGLObject source = RPGLFactory.newObject("debug:dummy");
+        RPGLObject target = RPGLFactory.newObject("debug:dummy");
+        RPGLContext context = new DummyContext();
+        context.add(source);
+        context.add(target);
+
+        AbilityContest abilityContest = new AbilityContest();
+        abilityContest.joinSubeventData(new JsonObject() {{
+            this.putJsonArray("fail", new JsonArray() {{
+                this.addJsonObject(new JsonObject() {{
+                    this.putString("subevent", "dummy_subevent");
+                }});
+            }});
+        }});
+        abilityContest.setSource(source);
+        abilityContest.setTarget(target);
+
+        abilityContest.resolveNestedSubevents("pass", context);
+
+        assertEquals(0, DummySubevent.counter,
+                "dummy subevent should not be invoked on pass"
+        );
+    }
+
+    @Test
+    @DisplayName("getSourceAbilityCheck returns source ability check result")
+    void getSourceAbilityCheck_returnsSourceAbilityCheckResult() throws Exception {
+        RPGLObject source = RPGLFactory.newObject("debug:dummy");
+        RPGLObject target = RPGLFactory.newObject("debug:dummy");
+        RPGLContext context = new DummyContext();
+        context.add(source);
+        context.add(target);
+
+        source.getAbilityScores().putInteger("wis", 20);
+
+        AbilityContest abilityContest = new AbilityContest();
+        abilityContest.joinSubeventData(new JsonObject() {{
+            /*{
+                "source_check": {
+                    "ability": "wis",
+                    "skill": "perception",
+                    "determined": [ 20 ]
+                }
+            }*/
+            this.putJsonObject("source_check", new JsonObject() {{
+                this.putString("ability", "wis");
+                this.putString("skill", "perception");
+                this.putJsonArray("determined", new JsonArray() {{
+                    this.addInteger(20);
+                }});
+            }});
+        }});
+        abilityContest.setSource(source);
+        abilityContest.setTarget(target);
+
+        assertEquals(25, abilityContest.getSourceAbilityCheck(context),
+                "source ability check should evaluate to 25 (20+5)"
+        );
+    }
+
+    @Test
+    @DisplayName("getTargetAbilityCheck returns target ability check result")
+    void getTargetAbilityCheck_returnsTargetAbilityCheckResult() throws Exception {
+        RPGLObject source = RPGLFactory.newObject("debug:dummy");
+        RPGLObject target = RPGLFactory.newObject("debug:dummy");
+        RPGLContext context = new DummyContext();
+        context.add(source);
+        context.add(target);
+
+        target.getAbilityScores().putInteger("wis", 20);
+
+        AbilityContest abilityContest = new AbilityContest();
+        abilityContest.joinSubeventData(new JsonObject() {{
+            /*{
+                "target_check": {
+                    "ability": "wis",
+                    "skill": "perception",
+                    "determined": [ 20 ]
+                }
+            }*/
+            this.putJsonObject("target_check", new JsonObject() {{
+                this.putString("ability", "wis");
+                this.putString("skill", "perception");
+                this.putJsonArray("determined", new JsonArray() {{
+                    this.addInteger(20);
+                }});
+            }});
+        }});
+        abilityContest.setSource(source);
+        abilityContest.setTarget(target);
+
+        assertEquals(25, abilityContest.getTargetAbilityCheck(context),
+                "target ability check should evaluate to 25 (20+5)"
+        );
+    }
+
+    @Test
+    @DisplayName("prepare prepares subevent")
+    void prepare_preparesSubevent() throws Exception {
+        RPGLObject source = RPGLFactory.newObject("debug:dummy");
+        RPGLContext context = new DummyContext();
+        context.add(source);
+
+        AbilityContest abilityContest = new AbilityContest();
+        abilityContest.joinSubeventData(new JsonObject());
+        abilityContest.setSource(source);
+        abilityContest.prepare(context);
+
+        assertTrue(abilityContest.getTags().asList().contains("ability_contest"),
+                "subevent should be given the ability_contest tag upon preparation"
+        );
+    }
+
+    @Test
+    @DisplayName("invoke invokes pass subevents (on pass)")
+    void invoke_invokesPassSubevents_onPass() throws Exception {
+        RPGLObject source = RPGLFactory.newObject("debug:dummy");
+        RPGLObject target = RPGLFactory.newObject("debug:dummy");
+        RPGLContext context = new DummyContext();
+        context.add(source);
+        context.add(target);
+
+        AbilityContest abilityContest = new AbilityContest();
+        abilityContest.joinSubeventData(new JsonObject() {{
+            /*{
+                "subevent": "ability_contest",
+                "source_check": {
+                    "ability": "wis",
+                    "skill": "perception",
+                    "determined": [ 20 ]
+                },
+                "target_check": {
+                    "ability": "wis",
+                    "skill": "perception",
+                    "determined": [ 1 ]
+                },
+                "pass": [
+                    { "subevent": "dummy_subevent" }
+                ]
+            }*/
+            this.putString("subevent", "ability_contest");
+            this.putJsonObject("source_check", new JsonObject() {{
+                this.putString("ability", "wis");
+                this.putString("skill", "perception");
+                this.putJsonArray("determined", new JsonArray() {{
+                    this.addInteger(20);
+                }});
+            }});
+            this.putJsonObject("target_check", new JsonObject() {{
+                this.putString("ability", "wis");
+                this.putString("skill", "perception");
+                this.putJsonArray("determined", new JsonArray() {{
+                    this.addInteger(1);
+                }});
+            }});
+            this.putJsonArray("pass", new JsonArray() {{
+                this.addJsonObject(new JsonObject() {{
+                    this.putString("subevent", "dummy_subevent");
+                }});
+            }});
+        }});
+        abilityContest.setSource(source);
+        abilityContest.prepare(context);
+        abilityContest.setTarget(target);
+        abilityContest.invoke(context);
+
+        assertEquals(1, DummySubevent.counter,
+                "dummy subevent should be invoked on pass"
+        );
+    }
+
+    @Test
+    @DisplayName("invoke does not invoke pass subevents (on fail)")
+    void invoke_doesNotInvokePassSubevents_onFail() throws Exception {
+        RPGLObject source = RPGLFactory.newObject("debug:dummy");
+        RPGLObject target = RPGLFactory.newObject("debug:dummy");
+        RPGLContext context = new DummyContext();
+        context.add(source);
+        context.add(target);
+
+        AbilityContest abilityContest = new AbilityContest();
+        abilityContest.joinSubeventData(new JsonObject() {{
+            /*{
+                "subevent": "ability_contest",
+                "source_check": {
+                    "ability": "wis",
+                    "skill": "perception",
+                    "determined": [ 1 ]
+                },
+                "target_check": {
+                    "ability": "wis",
+                    "skill": "perception",
+                    "determined": [ 20 ]
+                },
+                "pass": [
+                    { "subevent": "dummy_subevent" }
+                ]
+            }*/
+            this.putString("subevent", "ability_contest");
+            this.putJsonObject("source_check", new JsonObject() {{
+                this.putString("ability", "wis");
+                this.putString("skill", "perception");
+                this.putJsonArray("determined", new JsonArray() {{
+                    this.addInteger(1);
+                }});
+            }});
+            this.putJsonObject("target_check", new JsonObject() {{
+                this.putString("ability", "wis");
+                this.putString("skill", "perception");
+                this.putJsonArray("determined", new JsonArray() {{
+                    this.addInteger(20);
+                }});
+            }});
+            this.putJsonArray("pass", new JsonArray() {{
+                this.addJsonObject(new JsonObject() {{
+                    this.putString("subevent", "dummy_subevent");
+                }});
+            }});
+        }});
+        abilityContest.setSource(source);
+        abilityContest.prepare(context);
+        abilityContest.setTarget(target);
+        abilityContest.invoke(context);
+
+        assertEquals(0, DummySubevent.counter,
+                "dummy subevent should not be invoked on fail"
+        );
+    }
+
+    @Test
+    @DisplayName("invoke invokes fail subevents (on fail)")
+    void invoke_invokesFailSubevents_onFail() throws Exception {
+        RPGLObject source = RPGLFactory.newObject("debug:dummy");
+        RPGLObject target = RPGLFactory.newObject("debug:dummy");
+        RPGLContext context = new DummyContext();
+        context.add(source);
+        context.add(target);
+
+        AbilityContest abilityContest = new AbilityContest();
+        abilityContest.joinSubeventData(new JsonObject() {{
+            /*{
+                "subevent": "ability_contest",
+                "source_check": {
+                    "ability": "wis",
+                    "skill": "perception",
+                    "determined": [ 1 ]
+                },
+                "target_check": {
+                    "ability": "wis",
+                    "skill": "perception",
+                    "determined": [ 20 ]
+                },
+                "fail": [
+                    { "subevent": "dummy_subevent" }
+                ]
+            }*/
+            this.putString("subevent", "ability_contest");
+            this.putJsonObject("source_check", new JsonObject() {{
+                this.putString("ability", "wis");
+                this.putString("skill", "perception");
+                this.putJsonArray("determined", new JsonArray() {{
+                    this.addInteger(1);
+                }});
+            }});
+            this.putJsonObject("target_check", new JsonObject() {{
+                this.putString("ability", "wis");
+                this.putString("skill", "perception");
+                this.putJsonArray("determined", new JsonArray() {{
+                    this.addInteger(20);
+                }});
+            }});
+            this.putJsonArray("fail", new JsonArray() {{
+                this.addJsonObject(new JsonObject() {{
+                    this.putString("subevent", "dummy_subevent");
+                }});
+            }});
+        }});
+        abilityContest.setSource(source);
+        abilityContest.prepare(context);
+        abilityContest.setTarget(target);
+        abilityContest.invoke(context);
+
+        assertEquals(1, DummySubevent.counter,
+                "dummy subevent should be invoked on fail"
+        );
+    }
+
+    @Test
+    @DisplayName("invoke does not invoke fail subevents (on pass)")
+    void invoke_doesNotInvokeFailSubevents_onPass() throws Exception {
+        RPGLObject source = RPGLFactory.newObject("debug:dummy");
+        RPGLObject target = RPGLFactory.newObject("debug:dummy");
+        RPGLContext context = new DummyContext();
+        context.add(source);
+        context.add(target);
+
+        AbilityContest abilityContest = new AbilityContest();
+        abilityContest.joinSubeventData(new JsonObject() {{
+            /*{
+                "subevent": "ability_contest",
+                "source_check": {
+                    "ability": "wis",
+                    "skill": "perception",
+                    "determined": [ 20 ]
+                },
+                "target_check": {
+                    "ability": "wis",
+                    "skill": "perception",
+                    "determined": [ 1 ]
+                },
+                "fail": [
+                    { "subevent": "dummy_subevent" }
+                ]
+            }*/
+            this.putString("subevent", "ability_contest");
+            this.putJsonObject("source_check", new JsonObject() {{
+                this.putString("ability", "wis");
+                this.putString("skill", "perception");
+                this.putJsonArray("determined", new JsonArray() {{
+                    this.addInteger(20);
+                }});
+            }});
+            this.putJsonObject("target_check", new JsonObject() {{
+                this.putString("ability", "wis");
+                this.putString("skill", "perception");
+                this.putJsonArray("determined", new JsonArray() {{
+                    this.addInteger(1);
+                }});
+            }});
+            this.putJsonArray("fail", new JsonArray() {{
+                this.addJsonObject(new JsonObject() {{
+                    this.putString("subevent", "dummy_subevent");
+                }});
+            }});
+        }});
+        abilityContest.setSource(source);
+        abilityContest.prepare(context);
+        abilityContest.setTarget(target);
+        abilityContest.invoke(context);
+
+        assertEquals(0, DummySubevent.counter,
+                "dummy subevent should not be invoked on pass"
+        );
+    }
+
+}
