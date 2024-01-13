49cb85ede97db61acc87899834c245d1ecc3d981
calvi <calvin@brianandkathi.com>
2024-01-12 20:28:58 -0500

    Added default damage type to AddDamage subevents.
    Updated unit tests accordingly.
    
    Updated to version number v0.19.10-beta.

--- src/test/java/org/rpgl/function/AddDamageTest.java
+++ src/test/java/org/rpgl/function/AddDamageTest.java
@@ -12,6 +12,7 @@ import org.rpgl.datapack.DatapackLoader;
 import org.rpgl.exception.FunctionMismatchException;
 import org.rpgl.json.JsonArray;
 import org.rpgl.json.JsonObject;
+import org.rpgl.subevent.CriticalHitDamageCollection;
 import org.rpgl.subevent.DamageCollection;
 import org.rpgl.testUtils.DummyContext;
 import org.rpgl.testUtils.TestUtils;
@@ -63,8 +64,8 @@ public class AddDamageTest {
     }
 
     @Test
-    @DisplayName("adds damage")
-    void addsDamage() throws Exception {
+    @DisplayName("adds typed damage (damage collection)")
+    void addsTypedDamage_damageCollection() throws Exception {
         RPGLObject object = RPGLFactory.newObject("debug:dummy", TestUtils.TEST_USER);
 
         DamageCollection damageCollection = new DamageCollection();
@@ -107,6 +108,189 @@ public class AddDamageTest {
         String expected = """
                 [{"bonus":2,"damage_type":"fire","dice":[{"determined":[3],"size":6}],"scale":{"denominator":1,"numerator":1,"round_up":false}}]""";
         assertEquals(expected, damageCollection.getDamageCollection().toString(),
+                "execute should add appropriate damage range to collection"
+        );
+    }
+
+    @Test
+    @DisplayName("adds typeless damage (damage collection)")
+    void addsTypelessDamage_damageCollection() throws Exception {
+        RPGLObject object = RPGLFactory.newObject("debug:dummy", TestUtils.TEST_USER);
+
+        DamageCollection damageCollection = new DamageCollection();
+        damageCollection.setSource(object);
+        damageCollection.prepare(new DummyContext(), List.of());
+        damageCollection.addDamage(new JsonObject() {{
+            /*{
+                "damage_type": "fire",
+                "dice": [ ],
+                "bonus": 5,
+                "scale": {
+                    "numerator": 1,
+                    "denominator": 1,
+                    "round_up": false
+                }
+            }*/
+            this.putString("damage_type", "fire");
+            this.putJsonArray("dice", new JsonArray());
+            this.putInteger("bonus", 5);
+            this.putJsonObject("scale", new JsonObject() {{
+                this.putInteger("numerator", 1);
+                this.putInteger("denominator", 1);
+                this.putBoolean("round_up", false);
+            }});
+        }});
+
+        new AddDamage().execute(null, damageCollection, new JsonObject() {{
+            /*{
+                "function": "add_damage",
+                "damage": [
+                    {
+                        "formula": "range",
+                        "dice": [
+                            { "count": 1, "size": 6, "determined": [ 3 ] }
+                        ],
+                        "bonus": 0
+                    }
+                ]
+            }*/
+            this.putString("function", "add_damage");
+            this.putJsonArray("damage", new JsonArray() {{
+                this.addJsonObject(new JsonObject() {{
+                    this.putString("formula", "range");
+                    this.putJsonArray("dice", new JsonArray() {{
+                        this.addJsonObject(new JsonObject() {{
+                            this.putInteger("count", 1);
+                            this.putInteger("size", 6);
+                            this.putJsonArray("determined", new JsonArray() {{
+                                this.addInteger(3);
+                            }});
+                        }});
+                    }});
+                    this.putInteger("bonus", 0);
+                }});
+            }});
+        }}, new DummyContext(), List.of());
+
+        String expected = """
+                [{"bonus":5,"damage_type":"fire","dice":[],"scale":{"denominator":1,"numerator":1,"round_up":false}},{"bonus":0,"damage_type":"fire","dice":[{"determined":[3],"size":6}],"scale":{"denominator":1,"numerator":1,"round_up":false}}]""";
+        assertEquals(expected, damageCollection.getDamageCollection().toString(),
+                "execute should add appropriate damage range to collection"
+        );
+    }
+
+    @Test
+    @DisplayName("adds typed damage (critical hit damage collection)")
+    void addsTypedDamage_criticalHitDamageCollection() throws Exception {
+        RPGLObject object = RPGLFactory.newObject("debug:dummy", TestUtils.TEST_USER);
+
+        CriticalHitDamageCollection criticalHitDamageCollection = new CriticalHitDamageCollection();
+        criticalHitDamageCollection.setSource(object);
+        criticalHitDamageCollection.prepare(new DummyContext(), List.of());
+
+        new AddDamage().execute(null, criticalHitDamageCollection, new JsonObject() {{
+            /*{
+                "function": "add_damage",
+                "damage": [
+                    {
+                        "formula": "range",
+                        "damage_type": "fire",
+                        "dice": [
+                            { "count": 1, "size": 6, "determined": [ 3 ] }
+                        ],
+                        "bonus": 2
+                    }
+                ]
+            }*/
+            this.putString("function", "add_damage");
+            this.putJsonArray("damage", new JsonArray() {{
+                this.addJsonObject(new JsonObject() {{
+                    this.putString("formula", "range");
+                    this.putString("damage_type", "fire");
+                    this.putJsonArray("dice", new JsonArray() {{
+                        this.addJsonObject(new JsonObject() {{
+                            this.putInteger("count", 1);
+                            this.putInteger("size", 6);
+                            this.putJsonArray("determined", new JsonArray() {{
+                                this.addInteger(3);
+                            }});
+                        }});
+                    }});
+                    this.putInteger("bonus", 2);
+                }});
+            }});
+        }}, new DummyContext(), List.of());
+
+        String expected = """
+                [{"bonus":2,"damage_type":"fire","dice":[{"determined":[3],"size":6}],"scale":{"denominator":1,"numerator":1,"round_up":false}}]""";
+        assertEquals(expected, criticalHitDamageCollection.getDamageCollection().toString(),
+                "execute should add appropriate damage range to collection"
+        );
+    }
+
+    @Test
+    @DisplayName("adds typeless damage (critical hit damage collection)")
+    void addsTypelessDamage_criticalHitDamageCollection() throws Exception {
+        RPGLObject object = RPGLFactory.newObject("debug:dummy", TestUtils.TEST_USER);
+
+        CriticalHitDamageCollection criticalHitDamageCollection = new CriticalHitDamageCollection();
+        criticalHitDamageCollection.setSource(object);
+        criticalHitDamageCollection.prepare(new DummyContext(), List.of());
+        criticalHitDamageCollection.addDamage(new JsonObject() {{
+            /*{
+                "damage_type": "fire",
+                "dice": [ ],
+                "bonus": 5,
+                "scale": {
+                    "numerator": 1,
+                    "denominator": 1,
+                    "round_up": false
+                }
+            }*/
+            this.putString("damage_type", "fire");
+            this.putJsonArray("dice", new JsonArray());
+            this.putInteger("bonus", 5);
+            this.putJsonObject("scale", new JsonObject() {{
+                this.putInteger("numerator", 1);
+                this.putInteger("denominator", 1);
+                this.putBoolean("round_up", false);
+            }});
+        }});
+
+        new AddDamage().execute(null, criticalHitDamageCollection, new JsonObject() {{
+            /*{
+                "function": "add_damage",
+                "damage": [
+                    {
+                        "formula": "range",
+                        "dice": [
+                            { "count": 1, "size": 6, "determined": [ 3 ] }
+                        ],
+                        "bonus": 0
+                    }
+                ]
+            }*/
+            this.putString("function", "add_damage");
+            this.putJsonArray("damage", new JsonArray() {{
+                this.addJsonObject(new JsonObject() {{
+                    this.putString("formula", "range");
+                    this.putJsonArray("dice", new JsonArray() {{
+                        this.addJsonObject(new JsonObject() {{
+                            this.putInteger("count", 1);
+                            this.putInteger("size", 6);
+                            this.putJsonArray("determined", new JsonArray() {{
+                                this.addInteger(3);
+                            }});
+                        }});
+                    }});
+                    this.putInteger("bonus", 0);
+                }});
+            }});
+        }}, new DummyContext(), List.of());
+
+        String expected = """
+                [{"bonus":5,"damage_type":"fire","dice":[],"scale":{"denominator":1,"numerator":1,"round_up":false}},{"bonus":0,"damage_type":"fire","dice":[{"determined":[3],"size":6}],"scale":{"denominator":1,"numerator":1,"round_up":false}}]""";
+        assertEquals(expected, criticalHitDamageCollection.getDamageCollection().toString(),
                 "execute should add appropriate damage range to collection"
         );
     }
