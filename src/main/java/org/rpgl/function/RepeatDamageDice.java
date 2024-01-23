5c22cb138f50cdde168de38b32da3fb6d9bf504f
calvi <calvin@brianandkathi.com>
2024-01-23 00:02:59 -0500

    Implemented RepeatDamageDice function.
    Updated unit tests accordingly.

---
+++ src/main/java/org/rpgl/function/RepeatDamageDice.java
@@ -0,0 +1,62 @@
+package org.rpgl.function;
+
+import org.rpgl.core.RPGLContext;
+import org.rpgl.core.RPGLEffect;
+import org.rpgl.core.RPGLResource;
+import org.rpgl.json.JsonArray;
+import org.rpgl.json.JsonObject;
+import org.rpgl.subevent.CriticalHitDamageCollection;
+import org.rpgl.subevent.DamageCollection;
+import org.rpgl.subevent.Subevent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * This function is dedicated to repeating the initial damage dice of a DamageCollection or a
+ * CriticalHitDamageCollection a set number of times. If the initial damage has no dice or does not exist, this function
+ * does nothing.
+ *
+ * @author Calvin Withun
+ */
+public class RepeatDamageDice extends Function {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RepeatDamageDice.class);
+
+    public RepeatDamageDice() {
+        super("repeat_damage_dice");
+    }
+
+    @Override
+    public void run(RPGLEffect effect, Subevent subevent, JsonObject functionJson, RPGLContext context,
+                    List<RPGLResource> resources) throws Exception {
+        if (subevent instanceof DamageCollection damageCollection) {
+            repeatDamageDice(damageCollection.getDamageCollection(), functionJson);
+        } else if (subevent instanceof CriticalHitDamageCollection criticalHitDamageCollection) {
+            repeatDamageDice(criticalHitDamageCollection.getDamageCollection(), functionJson);
+        } else {
+            LOGGER.warn("Can not execute function on " + subevent.getClass());
+        }
+    }
+
+    /**
+     * This helper method is responsible for adding any extra dice provided to eligible subevents by this function.
+     *
+     * @param damageArray the damage array of an eligible subevent
+     * @param functionJson the function's JSON instructions
+     */
+    void repeatDamageDice(JsonArray damageArray, JsonObject functionJson) {
+        if (!damageArray.asList().isEmpty()) {
+            JsonObject damageElement = damageArray.getJsonObject(0);
+            JsonArray dice = damageElement.getJsonArray("dice");
+            if (!dice.asList().isEmpty()) {
+                JsonObject die = dice.getJsonObject(0);
+                for (int i = 0; i < Objects.requireNonNullElse(functionJson.getInteger("count"), 1); i++) {
+                    dice.addJsonObject(die.deepClone());
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/org/rpgl/function/RepeatDamageDiceTest.java
@@ -0,0 +1,304 @@
+package org.rpgl.function;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.rpgl.core.RPGLCore;
+import org.rpgl.core.RPGLFactory;
+import org.rpgl.core.RPGLObject;
+import org.rpgl.datapack.DatapackLoader;
+import org.rpgl.exception.FunctionMismatchException;
+import org.rpgl.json.JsonArray;
+import org.rpgl.json.JsonObject;
+import org.rpgl.subevent.CriticalHitDamageCollection;
+import org.rpgl.subevent.DamageCollection;
+import org.rpgl.testUtils.DummyContext;
+import org.rpgl.testUtils.TestUtils;
+import org.rpgl.uuidtable.UUIDTable;
+
+import java.io.File;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Testing class for the org.rpgl.function.RepeatDamageDice class.
+ *
+ * @author Calvin Withun
+ */
+public class RepeatDamageDiceTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        DatapackLoader.loadDatapacks(
+                new File("src/test/resources/datapacks".replace("/", File.separator))
+        );
+        RPGLCore.initializeTesting();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        DatapackLoader.DATAPACKS.clear();
+    }
+
+    @AfterEach
+    void afterEach() {
+        UUIDTable.clear();
+    }
+
+    @Test
+    @DisplayName("errors on wrong function")
+    void errorsOnWrongFunction() {
+        assertThrows(FunctionMismatchException.class,
+                () -> new RepeatDamageDice().execute(null, null, new JsonObject() {{
+                    /*{
+                        "function": "not_a_function"
+                    }*/
+                    this.putString("function", "not_a_function");
+                }}, new DummyContext(), List.of()),
+                "Function should throw a FunctionMismatchException if the specified function doesn't match"
+        );
+    }
+
+    @Test
+    @DisplayName("repeats damage dice (no count specified)")
+    void repeatsDamageDice_noCountSpecified() throws Exception {
+        RPGLObject object = RPGLFactory.newObject("debug:dummy", TestUtils.TEST_USER);
+
+        DamageCollection damageCollection = new DamageCollection();
+        damageCollection.joinSubeventData(new JsonObject() {{
+            /*{
+                "damage": [
+                    {
+                        "formula": "range",
+                        "damage_type": "fire",
+                        "dice": [
+                            { "count": 1, "size": 10, "determined": [ 5 ] }
+                        ],
+                        "bonus": 0
+                    }
+                ]
+            }*/
+            this.putJsonArray("damage", new JsonArray() {{
+                this.addJsonObject(new JsonObject() {{
+                    this.putString("formula", "range");
+                    this.putString("damage_type", "fire");
+                    this.putJsonArray("dice", new JsonArray() {{
+                        this.addJsonObject(new JsonObject() {{
+                            this.putInteger("count", 1);
+                            this.putInteger("size", 10);
+                            this.putJsonArray("determined", new JsonArray() {{
+                                this.addInteger(5);
+                            }});
+                        }});
+                    }});
+                    this.putInteger("bonus", 0);
+                }});
+            }});
+        }});
+        damageCollection.setSource(object);
+        damageCollection.prepare(new DummyContext(), List.of());
+
+        new RepeatDamageDice().execute(null, damageCollection, new JsonObject() {{
+            /*{
+                "function": "repeat_damage_dice"
+            }*/
+            this.putString("function", "repeat_damage_dice");
+        }}, new DummyContext(), List.of());
+
+        String expected = """
+                [{"bonus":0,"damage_type":"fire","dice":[{"determined":[5],"size":10},{"determined":[5],"size":10}],"scale":{"denominator":1,"numerator":1,"round_up":false}}]""";
+        assertEquals(expected, damageCollection.getDamageCollection().toString(),
+                "damage die should be repeated one time by default"
+        );
+    }
+
+    @Test
+    @DisplayName("repeats damage dice (count specified)")
+    void repeatsDamageDice_countSpecified() throws Exception {
+        RPGLObject object = RPGLFactory.newObject("debug:dummy", TestUtils.TEST_USER);
+
+        DamageCollection damageCollection = new DamageCollection();
+        damageCollection.joinSubeventData(new JsonObject() {{
+            /*{
+                "damage": [
+                    {
+                        "formula": "range",
+                        "damage_type": "fire",
+                        "dice": [
+                            { "count": 1, "size": 10, "determined": [ 5 ] }
+                        ],
+                        "bonus": 0
+                    }
+                ]
+            }*/
+            this.putJsonArray("damage", new JsonArray() {{
+                this.addJsonObject(new JsonObject() {{
+                    this.putString("formula", "range");
+                    this.putString("damage_type", "fire");
+                    this.putJsonArray("dice", new JsonArray() {{
+                        this.addJsonObject(new JsonObject() {{
+                            this.putInteger("count", 1);
+                            this.putInteger("size", 10);
+                            this.putJsonArray("determined", new JsonArray() {{
+                                this.addInteger(5);
+                            }});
+                        }});
+                    }});
+                    this.putInteger("bonus", 0);
+                }});
+            }});
+        }});
+        damageCollection.setSource(object);
+        damageCollection.prepare(new DummyContext(), List.of());
+
+        new RepeatDamageDice().execute(null, damageCollection, new JsonObject() {{
+            /*{
+                "function": "repeat_damage_dice",
+                "count": 2
+            }*/
+            this.putString("function", "repeat_damage_dice");
+            this.putInteger("count", 2);
+        }}, new DummyContext(), List.of());
+
+        String expected = """
+                [{"bonus":0,"damage_type":"fire","dice":[{"determined":[5],"size":10},{"determined":[5],"size":10},{"determined":[5],"size":10}],"scale":{"denominator":1,"numerator":1,"round_up":false}}]""";
+        assertEquals(expected, damageCollection.getDamageCollection().toString(),
+                "damage die should be repeated the specified number of times"
+        );
+    }
+
+    @Test
+    @DisplayName("repeats damage dice (critical hit damage collection)")
+    void repeatsDamageDice_criticalHitDamageCollection() throws Exception {
+        RPGLObject object = RPGLFactory.newObject("debug:dummy", TestUtils.TEST_USER);
+
+        CriticalHitDamageCollection criticalHitDamageCollection = new CriticalHitDamageCollection();
+        criticalHitDamageCollection.joinSubeventData(new JsonObject() {{
+            /*{
+                "damage": [
+                    {
+                        "damage_type": "fire",
+                        "dice": [
+                            { "size": 10, "determined": [ 5 ] }
+                        ],
+                        "bonus": 0,
+                        "scale": {
+                            "numerator": 1,
+                            "denominator": 1,
+                            "round_up": false
+                        }
+                    }
+                ]
+            }*/
+            this.putJsonArray("damage", new JsonArray() {{
+                this.addJsonObject(new JsonObject() {{
+                    this.putString("damage_type", "fire");
+                    this.putJsonArray("dice", new JsonArray() {{
+                        this.addJsonObject(new JsonObject() {{
+                            this.putInteger("size", 10);
+                            this.putJsonArray("determined", new JsonArray() {{
+                                this.addInteger(5);
+                            }});
+                        }});
+                    }});
+                    this.putInteger("bonus", 0);
+                    this.putJsonObject("scale", new JsonObject() {{
+                        this.putInteger("numerator", 1);
+                        this.putInteger("denominator", 1);
+                        this.putBoolean("round_up", false);
+                    }});
+                }});
+            }});
+        }});
+        criticalHitDamageCollection.setSource(object);
+        criticalHitDamageCollection.prepare(new DummyContext(), List.of());
+
+        new RepeatDamageDice().execute(null, criticalHitDamageCollection, new JsonObject() {{
+            /*{
+                "function": "repeat_damage_dice",
+                "count": 2
+            }*/
+            this.putString("function", "repeat_damage_dice");
+            this.putInteger("count", 2);
+        }}, new DummyContext(), List.of());
+
+        String expected = """
+                [{"bonus":0,"damage_type":"fire","dice":[{"determined":[5],"size":10},{"determined":[5],"size":10},{"determined":[5],"size":10}],"scale":{"denominator":1,"numerator":1,"round_up":false}}]""";
+        assertEquals(expected, criticalHitDamageCollection.getDamageCollection().toString(),
+                "damage die should be repeated the specified number of times"
+        );
+    }
+
+    @Test
+    @DisplayName("does nothing without damage")
+    void doesNothingWithoutDamage() throws Exception {
+        RPGLObject object = RPGLFactory.newObject("debug:dummy", TestUtils.TEST_USER);
+
+        DamageCollection damageCollection = new DamageCollection();
+        damageCollection.setSource(object);
+        damageCollection.prepare(new DummyContext(), List.of());
+
+        new RepeatDamageDice().execute(null, damageCollection, new JsonObject() {{
+            /*{
+                "function": "repeat_damage_dice",
+                "count": 2
+            }*/
+            this.putString("function", "repeat_damage_dice");
+            this.putInteger("count", 2);
+        }}, new DummyContext(), List.of());
+
+        assertEquals("[]", damageCollection.getDamageCollection().toString(),
+                "function should do nothing when no initial damage is present"
+        );
+    }
+
+    @Test
+    @DisplayName("does nothing without damage dice")
+    void doesNothingWithoutDamageDice() throws Exception {
+        RPGLObject object = RPGLFactory.newObject("debug:dummy", TestUtils.TEST_USER);
+
+        DamageCollection damageCollection = new DamageCollection();
+        damageCollection.joinSubeventData(new JsonObject() {{
+            /*{
+                "damage": [
+                    {
+                        "formula": "range",
+                        "damage_type": "fire",
+                        "dice": [ ],
+                        "bonus": 2
+                    }
+                ]
+            }*/
+            this.putJsonArray("damage", new JsonArray() {{
+                this.addJsonObject(new JsonObject() {{
+                    this.putString("formula", "range");
+                    this.putString("damage_type", "fire");
+                    this.putJsonArray("dice", new JsonArray());
+                    this.putInteger("bonus", 2);
+                }});
+            }});
+        }});
+        damageCollection.setSource(object);
+        damageCollection.prepare(new DummyContext(), List.of());
+
+        new RepeatDamageDice().execute(null, damageCollection, new JsonObject() {{
+            /*{
+                "function": "repeat_damage_dice",
+                "count": 2
+            }*/
+            this.putString("function", "repeat_damage_dice");
+            this.putInteger("count", 2);
+        }}, new DummyContext(), List.of());
+
+        String expected = """
+                [{"bonus":2,"damage_type":"fire","dice":[],"scale":{"denominator":1,"numerator":1,"round_up":false}}]""";
+        assertEquals(expected, damageCollection.getDamageCollection().toString(),
+                "function should do nothing when initial damage does not include dice"
+        );
+    }
+
+}
